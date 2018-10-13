923722e66cf229a152cdb36f813a51488271988d
Blake Sager <blake.sager@activeprospect.com>
2018-10-13 15:16:43 -0500

    Add comments explaining execution order

--- node-internals/multitask.js
+++ node-internals/multitask.js
@@ -32,3 +32,12 @@ doHash();
 doHash();
 doHash();
 doHash();
+
+
+// ➜  node-internals git:(master) ✗ node multitask.js
+// Request:  330  milliseconds => executed by OS (outside of thread pool)
+// Hash:  1010  milliseconds => executed in-between the two HD access requests (inside of thread pool)
+// FS:  1011  milliseconds => executed after second HD request and after thread from prev hash is freed up (inside of thread pool)
+// Hash:  1026  milliseconds => inside of thread pool
+// Hash:  1034  milliseconds => inside of thread pool
+// Hash:  1049  milliseconds => inside of thread pool
