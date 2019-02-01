445571fb1b0503e25b997877e3bd8d933ba1420d
Blake Sager <blake.sager@activeprospect.com>
2019-02-01 15:12:16 -0600

    add comment

--- performant-node/index.js
+++ performant-node/index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1; // child process have only one thread
 const cluster = require('cluster');
 
 if (cluster.isMaster) {
@@ -5,20 +6,16 @@ if (cluster.isMaster) {
   cluster.fork();
   // cluster.fork();
   // cluster.fork();
-  // cluster.fork();
 } else {  
   // child process, just a server
   const express = require('express');
   const app = express();
-  
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-  }
+  const crypto = require('crypto');
   
   app.get('/', (req, res) => {
-    doWork(5000); // event loop blocked
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
   });
 
   app.get('/fast', (req, res) => {
