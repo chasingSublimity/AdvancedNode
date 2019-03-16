eaf76ec6bccaf94dc307225463861750801ec586
Blake Sager <blake.sager@activeprospect.com>
2019-03-16 12:12:50 -0500

    finish pm2

--- performant-node/index.js
+++ performant-node/index.js
@@ -1,26 +1,16 @@
-process.env.UV_THREADPOOL_SIZE = 1; // child process have only one thread
-const cluster = require('cluster');
+// child process, just a server
+const express = require('express');
+const app = express();
+const crypto = require('crypto');
 
-if (cluster.isMaster) {
-  // execute index.js again in child mode
-  cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-} else {  
-  // child process, just a server
-  const express = require('express');
-  const app = express();
-  const crypto = require('crypto');
-  
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send('Hi there');
-    });
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hi there');
   });
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('This was fast!');
-  });
-  
-  app.listen(3000);
-}
+app.get('/fast', (req, res) => {
+  res.send('This was fast!');
+});
+
+app.listen(3000);
